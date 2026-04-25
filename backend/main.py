260a16d265027c3b69ddcfb18c8b211d3e50d916
Njiboy <njisylven51@gmail.com>
2026-04-25 13:55:20 +0100

    created the database

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from database import engine, Base
+
+import models
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def root():
+    return {"message": "Backend Running"}
