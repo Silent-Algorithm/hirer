4d6108412f2a026fe1ec152c3ea89ddaa742586d
Gama Tuh Micheal Glenn <tohmichael62@gmail.com>
2026-04-26 20:28:03 +0100

    now if user press or clicks and hold the screen, the taskbar will dissapear, and appear back when the release the click/press. i have completed styling for the search page and made it responsive

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,15 +1,14 @@
-
-
+//Theme mode
 const toggleTheme = document.getElementById('toggleTheme');
 
 if (toggleTheme) {
-    
+    //toggle dark and ligh theme
     toggleTheme.addEventListener('click', () => {
         document.documentElement.classList.toggle('dark')
         console.log(document.documentElement.classList.value)
         localStorage.setItem("theme",document.documentElement.classList.value)
     })
-
+//render post
     const feet = document.getElementById('feet')
     const feetSection = document.getElementById('feetSection')
 
@@ -20,33 +19,35 @@ if (toggleTheme) {
     }
    
 }
+
     
-//toggle taskbar during scroll
+//toggle taskbar during scroll and screen holds
    let lastScrollY = scrollY;
     
 const footer = document.querySelector('footer')
 const footerSection = document.getElementById('footerSection')
 
-window.document.addEventListener('scroll', () => {
+let timer
+const startHold = () => {
+    timer = setTimeout(() => {
+        footer.style.display = 'none'
+    },400)
+}
 
-    if (scrollY - lastScrollY >150 || lastScrollY - scrollY >150) {
-            
-       footer.style.display = 'none'
-    footerSection.display= 'none' 
-        lastScrollY = scrollY;
-    }
-    
-})
-
-window.document.addEventListener('scrollend', () => {
+const endHold = () => {
     footer.style.display = 'flex'
-    footerSection.style.display = 'flex'
-})
+    clearTimeout(timer)
+}
+window.document.addEventListener('pointerdown',startHold);
+window.document.addEventListener('pointerup',endHold)
+
+window.document.addEventListener('pointerleave', endHold)
+
 
 //use the same theme in the home page accross all pages
 localStorage.getItem("theme") === 'dark' ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark');
 
-//liten to taskbar icon clicks
+//liten to taskbar's icon clicks
 
 const searchIcon = document.getElementById('searchIcon')
 const homeIcon = document.getElementById('homeIcon')
@@ -73,9 +74,9 @@ createIcon.addEventListener('click', (event) => {
 //track clicked taskbar icon
 
 const page = document.querySelector('title')
-console.log(page.id)
 
 const styleTaskbarIcon = () => {
+
     const footerIcon = document.getElementById('footerIcon');
     const footerIconText = document.getElementById('footerIconText');
 
@@ -84,3 +85,23 @@ const styleTaskbarIcon = () => {
 }
 
 page.id === 'homeIconPage'?styleTaskbarIcon():page.id === 'searchIconPage'?styleTaskbarIcon():page.id === 'createIconPage'?styleTaskbarIcon(): ' '
+
+//render Search page category and recommendation cards
+
+const recommendationCard = document.getElementById('recommendationCard')
+const recommendationSection = document.getElementById('recommendationSection')
+const categoryCard = document.getElementById('categoryCard')
+const categorySection = document.getElementById('categorySection')
+
+if (recommendationCard || categoryCard) {
+    for (let i = 0; i < 4; i++){
+        let newRecommendationCard = document.createElement('div');
+        newRecommendationCard.innerHTML = recommendationCard.innerHTML;
+        recommendationSection.append(newRecommendationCard);
+
+         let newCategoryCard = document.createElement('div');
+        newCategoryCard.innerHTML = categoryCard.innerHTML;
+        categorySection.append(newCategoryCard);
+
+    }
+}
