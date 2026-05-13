aab6ee575d3f98e0ac1543ecc9f4a007d3cdf3da
Gama Tuh Micheal Glenn <tohmichael62@gmail.com>
2026-05-13 22:19:58 +0100

    i have fixed the styling issue in the favorite page; i have restyle the footer and hearder  to work in sync with the other pages(footer). i have link he page favorite page to script.js and change the styling to vite to work offline

--- Frontend/home.js
+++ Frontend/home.js
@@ -1,6 +1,8 @@
-const bookmarkIcon = document.getElementsByClassName('bookmarkIcon')
-console.log(bookmarkIcon)
-bookmarkIcon.addEventListener('click', () => {
-    bookmarkIcon.style.fill = 'blue'
-    console.log('clicked bookmark')
-})
+const bookmarkIcon = document.querySelectorAll('.bookmarkIcon')
+// bookmarkIcon.addEventListener('click', () => {
+//     bookmarkIcon.style.fill = 'blue'
+//     console.log('clicked bookmark')
+// })
+
+// console.log(bookmarkIcon)
+
